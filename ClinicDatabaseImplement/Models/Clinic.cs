ebba155c3837166b18962658e887351931f17f3c
Vikabraunone <vikabraunone@gmail.com>
2020-06-23 11:06:33 +0400

    1.1

---
+++ ClinicDatabaseImplement/Models/Clinic.cs
@@ -0,0 +1,11 @@
+﻿namespace ClinicDatabaseImplement.Models
+{
+    public class Clinic
+    {
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+
+        public string Address { get; set; }
+    }
+}
